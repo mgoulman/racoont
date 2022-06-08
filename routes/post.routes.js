60bee5ce2292578aeef3ba3c1d781475f116e3d3
mgoulman <moadgoulman@gmail.com>
2022-06-08 02:47:08 +0100

    finishin

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -8,4 +8,10 @@ router.delete('/:id', postController.deletePost);
 router.patch('/like-post/:id', postController.likePost);
 router.patch('/unlike-post/:id', postController.unlikePost);
 
+// comments
+
+router.patch('/comment-post/:id', postController.commentPost);
+router.patch('/edit-comment-post/:id', postController.editCommentPost);
+router.delete('/delete-comment-post/:id', postController.deleteCommentPost);
+
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
+const uploadController = require("../controllers/upload.controller");
+const multer = require('multer');
+const upload = multer();
+
 
 //auth
 router.post("/register", authController.signUp);
@@ -14,5 +18,9 @@ router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unfollow);
+
+// upload
+
+router.post('/upload', upload.single('file'), uploadController.uploadProfile)
 
 module.exports = router;

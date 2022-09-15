835a1a038424d90a6bb93b5ca9395ba334d3df07
mglouman <moadgoulman@gmail.com>
2022-09-15 01:50:51 +0100

    Edited, card UI

--- client/src/components/Post/Card.js
+++ client/src/components/Post/Card.js
@@ -1,19 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { isEmpty } from "../Utils";
+import { isEmpty, parseDate } from "../Utils";
 
 export default function Card({ post }) {
   const [isLoading, setIsLoading] = useState(true);
   const usersData = useSelector((state) => state.usersReducer);
   const userData = useSelector((state) => state.userReducer);
+  let pic;
 
   useEffect(() => {
     !isEmpty(usersData[0]) && setIsLoading(false);
   }, [usersData]);
-  
+
   return (
     <li className="card-container" key={post._id}>
-      {isLoading ? <i className="fas fa-spinner fa-spin"></i> : <h2>test</h2>}
+      {isLoading ? (
+        <i className="fas fa-spinner fa-spin"></i>
+      ) : (
+        <>
+          <div className="card-left">
+            <img
+              src={
+                !isEmpty(usersData[0]) &&
+                usersData
+                  .map((user) => {
+                    if (user._id === post.posterId) return user.picture;
+                    else return null;
+                  })
+                  .join("")
+              }
+              alt="poster-pic"
+            />
+          </div>
+          <div className="card-right">
+            <div className="card-header">
+              <div className="pseudo">
+                <h3>{
+                    !isEmpty(usersData[0]) &&
+                    usersData
+                      .map((user) => {
+                        if (user._id === post.posterId) return user.pseudo;
+                        else return null;
+                      })
+                  }</h3>
+              </div>
+              <span>{parseDate(post.createdAt)}</span>
+            </div>
+          </div>
+        </>
+      )}
     </li>
   );
 }
